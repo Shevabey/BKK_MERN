29e43e1e262917c6a9400ca256e1cf5884d80e16
Shevabey <Shevabey100@gmail.com>
2024-07-10 22:50:43 +0700

    Update DB

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,27 @@
 import { Sequelize } from "sequelize";
+// const { Sequelize } = require("sequelize");
 
-const db = new Sequelize("bkk_db", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const db = new Sequelize(
+  process.env.DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
+
+// const db = new Sequelize({
+//   username: process.env.DB_USERNAME,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DATABASE,
+//   dialect: "mysql",
+//   port: process.env.APP_PORT,
+//   host: process.env.DB_HOST,
+// });
 
 export default db;
